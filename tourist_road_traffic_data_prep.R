027a9851431b1baa6dc36b6dba81c00a450dbde2
Snorre Hansen <snorre.hansen@vegvesen.no>
2021-06-29 12:48:39 +0200

    Chosen trps

--- tourist_road_traffic_data_prep.R
+++ tourist_road_traffic_data_prep.R
@@ -52,7 +52,7 @@ trps_on_tourist_roads <- points_metadata %>%
   dplyr::mutate(name = stringr::str_replace_all(name, "E10 ", ""),
                 name = stringr::str_replace_all(name, " \\(Fv27\\)", ""),
                 name = stringr::str_replace_all(name, " \\(Fv51\\)", ""),
-                name = stringr::str_replace_all(name, " \\(Fv82\\)", ""),
+                name = stringr::str_replace_all(name, "Fv82 ", ""),
                 road_category_and_number_and_point_name =
                   paste0(road_category_and_number, " ", name))
 
@@ -142,6 +142,52 @@ pis <- dplyr::bind_rows(
                 base_volume, calculation_volume) %>%
   dplyr::left_join(trp_id_objekt_id, by = "trp_id")
 
+pis %>%
+  saveRDS(file = "data/pis.rds")
+
+# TRPs with most traffic and their July data ----
+trp_most_traffic <- trp_mdt_long %>%
+  dplyr::mutate(month = lubridate::month(month_object)) %>%
+  dplyr::filter(year %in% c(2020),
+                month == 7) %>%
+  dplyr::left_join(pis, by = c("trp_id", "year", "month")) %>%
+  dplyr::filter(!is.na(index_total_p)) %>%
+  dplyr::group_by(Navn) %>%
+  dplyr::slice_max(order_by = mdt)
+
+# Must add Lappestein
+lappestein <- trp_mdt_long %>%
+  dplyr::mutate(month = lubridate::month(month_object)) %>%
+  dplyr::filter(year %in% c(2020),
+                month == 7,
+                trp_id == "62909V181278")
+
+trp_most_traffic_2020 <- dplyr::bind_rows(
+  trp_most_traffic,
+  lappestein
+)
+
+
+trp_chosen <- trp_mdt_long %>%
+  dplyr::mutate(month = lubridate::month(month_object)) %>%
+  dplyr::filter(year %in% c(2020, 2021),
+                month == 7,
+                trp_id %in% trp_most_traffic_2020$trp_id) %>%
+  dplyr::left_join(pis, by = c("trp_id", "year", "month")) %>%
+  dplyr::select(
+    #objekt_id,
+    Navn,
+    #trp_id,
+    road_category_and_number_and_point_name,
+    year,
+    #month,
+    mdt, index = index_total_p) %>%
+  tidyr::pivot_wider(names_from = year, values_from = c(mdt, index)) %>%
+  dplyr::arrange(Navn) %>%
+  dplyr::relocate(mdt_2021, .before = index_2021)
+
+trp_chosen %>%
+  saveRDS(file = "data/trp_chosen.rds")
 
 # Tourist road index ----
 ## Per road ----
@@ -231,3 +277,86 @@ index_summer_and_month <- index_per_month %>%
 
 index_summer_and_month %>%
   saveRDS(file = "data/index_summer_and_month.rds")
+
+
+# Other summer trps ----
+summer_trps <- tibble::tibble(
+  trp_id = c(
+    "97410V1751020",
+    "03556V2741763",
+    "22500V805651",
+    "04474V1060615",
+    "24362V72144",
+    "39676V885922",
+    "34573V930302",
+    "04904V971774"
+  ),
+  nearby_name = c(
+    "Lillesand",
+    "Lærdal - Valdres",
+    "Sørfjorden i Hardanger",
+    "Lillehammer",
+    "Dovrefjell",
+    "Saltfjellet",
+    "Nordkapp",
+    "Svenskegrensa"
+  )
+)
+
+
+summer_trps_meta_data <- points_metadata %>%
+  dplyr::filter(trp_id %in% summer_trps$trp_id) %>%
+  dplyr::distinct(trp_id, .keep_all = TRUE) %>%
+  dplyr::mutate(name = stringr::str_replace_all(name, " E69", ""),
+                road_category_and_number_and_point_name =
+                  paste0(road_category_and_number, " ", name)) %>%
+  dplyr::left_join(summer_trps, by = "trp_id") %>%
+  dplyr::select(trp_id, road_category_and_number_and_point_name, nearby_name)
+
+summer_mdt_2020 <- get_mdt_for_trp_list(summer_trps_meta_data$trp_id, "2020")
+summer_mdt_2021 <- get_mdt_for_trp_list(summer_trps_meta_data$trp_id, "2021")
+
+summer_mdts <- dplyr::bind_rows(
+  summer_mdt_2020,
+  summer_mdt_2021
+) %>%
+  dplyr::filter(coverage > 50) %>%
+  dplyr::select(trp_id, year, month, mdt) %>%
+  tidyr::complete(trp_id = summer_trps_meta_data$trp_id, year, month)
+
+summer_trp_mdt_long <- summer_trps_meta_data %>%
+  dplyr::left_join(summer_mdts, by = "trp_id") %>%
+  dplyr::mutate(month_object = lubridate::make_date(year = 2000, month = month),
+                month_name = lubridate::month(month_object, label = TRUE, abbr = FALSE)) %>%
+  dplyr::select(trp_id, road_category_and_number_and_point_name, nearby_name,
+                year, month_object, mdt)
+
+summer_pi_2020 <- get_pointindices_for_trp_list(summer_trps_meta_data$trp_id, "2020")
+summer_pi_2021 <- get_pointindices_for_trp_list(summer_trps_meta_data$trp_id, "2021")
+
+summer_pis <- dplyr::bind_rows(
+  summer_pi_2020,
+  summer_pi_2021
+) %>%
+  dplyr::filter(month %in% c(6, 7, 8),
+                day_type == "ALL",
+                period == "month") %>%
+  dplyr::select(trp_id, year, month, index = index_total_p)
+
+summer_trp_chosen <- summer_trp_mdt_long %>%
+  dplyr::mutate(month = lubridate::month(month_object)) %>%
+  dplyr::filter(year %in% c(2020, 2021),
+                month == 7) %>%
+  dplyr::left_join(summer_pis, by = c("trp_id", "year", "month")) %>%
+  dplyr::select(
+    #trp_id,
+    road_category_and_number_and_point_name,
+    nearby_name,
+    year,
+    #month,
+    mdt, index) %>%
+  tidyr::pivot_wider(names_from = year, values_from = c(mdt, index)) %>%
+  dplyr::relocate(mdt_2021, .before = index_2021)
+
+summer_trp_chosen %>%
+  saveRDS(file = "data/summer_trp_chosen.rds")
